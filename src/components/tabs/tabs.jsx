73e2113627e2b6c049f5e84a12417900d38d5694
laksix <147379934+laksix@users.noreply.github.com>
2024-02-27 00:13:22 +0700

    Update tabs.jsx

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -1,1 +1,32 @@
-import React, { useState } from 'react';import { Tabs } from 'antd';import FilmsList from '../FilmsList/filmslist';import './tabs.css';const TabsPanel = ({ guestSession }) => {  const [currentTab, setCurrentTab] = useState('1');  const items = [    {      key: '1',      label: 'Search',      children: <FilmsList currentTab={currentTab} guestSession={guestSession} />,    },    {      key: '2',      label: 'Rated',      children: <FilmsList currentTab={currentTab} guestSession={guestSession} />,    },  ];  return (    <Tabs      onTabClick={(key) => {        setCurrentTab(key);      }}      defaultActiveKey="1"      items={items}      className="tabs-options"      size={'middle'}    />  );};export default TabsPanel;
+import React, { useState } from 'react';
+import { Tabs } from 'antd';
+import FilmsList from '../FilmsList/filmslist';
+import './tabs.css';
+const TabsPanel = ({ guestSession }) => {
+  const [currentTab, setCurrentTab] = useState('1');
+
+  const items = [
+    {
+      key: '1',
+      label: 'Search',
+      children: <FilmsList currentTab={currentTab} guestSession={guestSession} />,
+    },
+    {
+      key: '2',
+      label: 'Rated',
+      children: <FilmsList currentTab={currentTab} guestSession={guestSession} />,
+    },
+  ];
+  return (
+    <Tabs
+      onTabClick={(key) => {
+        setCurrentTab(key);
+      }}
+      defaultActiveKey="1"
+      items={items}
+      className="tabs-options"
+      size={'middle'}
+    />
+  );
+};
+export default TabsPanel;
