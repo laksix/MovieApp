09879a7c8e022dadb8961b438847afc1fdf17ec5
laksix <147379934+laksix@users.noreply.github.com>
2024-02-27 00:13:12 +0700

    Update pagination.jsx

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -1,1 +1,19 @@
-import React from 'react';import './pagination.css';import { Pagination } from 'antd';const PaginationType = ({ getCurrentPage, currentPage, totalPages }) => {  return (    <div className="paginationSettings">      <Pagination        onChange={(page, pageSize) => getCurrentPage(page, pageSize)}        current={currentPage}        total={totalPages}        pageSize={20}        showSizeChanger={false}      />    </div>  );};export default PaginationType;
+import React from 'react';
+import './pagination.css';
+
+import { Pagination } from 'antd';
+
+const PaginationType = ({ getCurrentPage, currentPage, totalPages }) => {
+  return (
+    <div className="paginationSettings">
+      <Pagination
+        onChange={(page, pageSize) => getCurrentPage(page, pageSize)}
+        current={currentPage}
+        total={totalPages}
+        pageSize={20}
+        showSizeChanger={false}
+      />
+    </div>
+  );
+};
+export default PaginationType;
